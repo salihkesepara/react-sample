94a825c37fd43253d763ba15a61a6320e1a1dcc7
Salih Kesepara <salihkesepara@gmail.com>
2022-11-01 13:25:25 +0300

    login updated

--- src/views/login.js
+++ src/views/login.js
@@ -15,6 +15,7 @@ const Login = ({ userActions }) => {
 
   return (
     <Container>
+      <p>{process.env.REACT_APP_CUSTOM_NODE_ENV}</p>
       <button onClick={handleSuccessLogin}>Login</button>
     </Container>
   )
